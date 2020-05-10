3efaf1d35f7dcdc3d1f45ccef80d3287bfd9f450
Arthur Sultanov <as@boolqa.com>
2020-05-10 23:21:37 +0300

    fstream output added

--- ConsoleApplicationDM.cpp
+++ ConsoleApplicationDM.cpp
@@ -2,6 +2,7 @@
 #include <string>
 #include <vector>
 #include <cmath>
+#include <fstream>
 
 using vvs = std::vector<std::vector<std::string>>;
 using vs = std::vector<std::string>;
@@ -82,13 +83,23 @@ int main() {
   std::cout << "PDNF: " << getPDNF(funcVector, truthTable, columns) << std::endl;
   std::cout << "PCNF: " << getPCNF(funcVector, truthTable, columns) << std::endl << std::endl;
 
-  // n1 = 16 + 3 = 19; 0d19 = 0b10011;
-  std::string n1FuncVector = "00010011";
-  std::cout << "n1 (16 + 3) 00010011 PDNF: " << getPDNF(n1FuncVector, truthTable, columns) << std::endl;
-  std::cout << "n1 (16 + 3) 00010011 PCNF: " << getPCNF(n1FuncVector, truthTable, columns) << std::endl;
+  // n1 = 17 + 3 = 20; 0d20 = 0b10100;
+  std::string n1FuncVector = "00010100";
+  std::cout << "n1 (17 + 3) 00010100 PDNF: " << getPDNF(n1FuncVector, truthTable, columns) << std::endl;
+  std::cout << "n1 (17 + 3) 00010100 PCNF: " << getPCNF(n1FuncVector, truthTable, columns) << std::endl;
 
-  // n2 = 256 - 19 = 237; 0d237 = 0b11101101;
-  std::string n2FuncVector = "11101101";
-  std::cout << "n2 (256 - (16 + 3)) 11101101 PDNF: " << getPDNF(n2FuncVector, truthTable, columns) << std::endl;
-  std::cout << "n2 (256 - (16 + 3)) 11101101 PCNF: " << getPCNF(n2FuncVector, truthTable, columns) << std::endl;
+  // n2 = 256 - 20 = 236; 0d236 = 0b11101100;
+  std::string n2FuncVector = "11101100";
+  std::cout << "n2 (256 - (17 + 3)) 11101100 PDNF: " << getPDNF(n2FuncVector, truthTable, columns) << std::endl;
+  std::cout << "n2 (256 - (17 + 3)) 11101100 PCNF: " << getPCNF(n2FuncVector, truthTable, columns) << std::endl;
+
+  std::fstream output("output.txt", std::fstream::out);
+  output << "PDNF: " << getPDNF(funcVector, truthTable, columns) << std::endl;
+  output << "PCNF: " << getPCNF(funcVector, truthTable, columns) << std::endl << std::endl;
+
+  output << "n1 (17 + 3) 00010100 PDNF: " << getPDNF(n1FuncVector, truthTable, columns) << std::endl;
+  output << "n1 (17 + 3) 00010100 PCNF: " << getPCNF(n1FuncVector, truthTable, columns) << std::endl;
+
+  output << "n2 (256 - (17 + 3)) 11101100 PDNF: " << getPDNF(n2FuncVector, truthTable, columns) << std::endl;
+  output << "n2 (256 - (17 + 3)) 11101100 PCNF: " << getPCNF(n2FuncVector, truthTable, columns) << std::endl;
 }
